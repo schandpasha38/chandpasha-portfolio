63a5cf70017d98f3eebd0e0b59cf02a5679a49b5
Chand Pasha <schandpasha38@gmail.com>
2020-05-01 05:11:38 -0700

    1. Updated height propertiy of Head and Banner using viewHeight vh to make to appear in user viewport 2. Added Margin left and margin right of 30 in homebanner component to aline with header 3. Added dummy About component for testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Components/Header/Header'
 import HomeBanner from './Components/HomeBanner/HomeBanner';
+import About from './Components/About/About';
 
 function App() {
   return (
     <>
       <Header />
       <HomeBanner />
+      <About />
     </>
   );
 }
---
+++ src/Components/About/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './About.scss'
+
+function About() {
+    return (
+        <section className="about-section">
+            <div>
+                <h2>About Me</h2>
+            </div>
+            <div>
+                <p>My name is Chand Pasha I'm a 29 years old Front End Developer based in Bangalore. I describe myself as a developer who loves coding, open source, and the web platform. Currently I'm working at a company called Ajio, as a Front End Engineer.</p>
+                <p>In my spare time I like to create and contribute to open source projects. That helps me to learn a lots of new stuff, grow as a developer and support other open source projects. I love travelling and discovering new places.</p>
+                <p>Also, I enjoy writing at my blog and going to developer meetups and conferences around Barcelona.</p>
+            </div>
+        </section>
+    );
+};
+
+export default About;
