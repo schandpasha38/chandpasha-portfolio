f53e108a4e75133da80d85184cbb5cf1fb355537
Chand Pasha <schandpasha38@gmail.com>
2020-05-03 10:59:25 -0700

    1. Revamped Whole HTML and CSS file of about page 2. Removed bottom box-shadow from header 3. Made homepage image responsive

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './About.scss';
 import Header from '../Header/Header';
-import AboutChandPic from './assets/AboutChandPic.png'
+import AboutChandPic from './assets/AboutChandPic.jpg'
 import { Helmet } from "react-helmet";
 import gitIcon from './assets/svgicons/github.svg';
 import linkedInIcon from './assets/svgicons/linkedin.svg';
@@ -20,39 +20,56 @@ function About() {
             <Header />
             <section className="about-section">
                 <div className="about-wrapper">
+
                     <div className="about-headline">
-                        <h2>About Me</h2>
+                        <p className="about-discription">Get to know me</p>
+                        <h2 className="about-title">About Me</h2>
                     </div>
+
                     <div className="about-section-container">
-                        <section className="about-text-container">
-                            <div className="about-text">
-                                <p>My name is Chand Pasha I'm a 30 years old Freelance Front-end developer  based in Bengaluru. I describe myself as a developer who loves web technologies.</p>
-                                <p>Before I got into software, I worked for a ECommerce company for almost five years building websites with HTML, CSS, and JavaScript In my spare time While working full time, I was learning and growing my skills as a Front-End developer.</p>
-                                <h3>This are some Tech i use</h3>
-                                HTML, CSS, Sass, Bootstrap, Javascript (ES5, ES6), jQuery, React, Node.js, GIT, Express, MongoDB
-                            </div>
-                        </section>
                         <section className="about-pic-container">
-                            <img src={AboutChandPic} alt="" />
-                            <div className="about-social">
-                                <li><a href="https://www.github.com/schandpasha38" target="_blank" rel="noopener noreferrer"><img className="socialIcons" src={gitIcon} alt="" /></a></li>
-                                <li><a href="https://www.linkedin.com/in/chand-pasha-b6433889" target="_blank" rel="noopener noreferrer"><img className="socialIcons" src={linkedInIcon} alt="" /></a></li>
-                                <li><a href="https://www.twitter.com/itschandpasha" target="_blank" rel="noopener noreferrer"><img className="socialIcons" src={twitterIcon} alt="" /></a></li>
-                                <li><a href="mailto:schandpasha38@gmail.com" target="_blank" rel="noopener noreferrer"><img className="socialIcons" src={eMialIcon} alt="" /></a></li>
+                            <img className="about-pic-pic" src={AboutChandPic} alt="" />
+                        </section>
+                        <section className="about-text-container">
+                            <div className="about-text-head">
+                                <h6 className="about-text-title">I'm Chand Pasha, Freelance Front-end developer </h6>
+                                <div className="about-text-discription">
+                                    <p>I am Freelance Front-end developer based in Bengaluru. I describe myself as a developer who loves web technologies.</p>
+                                    <p>Before I got into software, I worked for a ECommerce company for almost five years building websites with HTML, CSS, and JavaScript In my spare time While working full time, I was learning and growing my skills as a Front-End developer.</p>
+                                </div>
+                            </div>
+                            <div className="about-text-details">
+                                <div className="about-text-info-row">
+                                    <div className="single-info"><span>Name:</span><p>Chand Pasha</p></div>
+                                    <div className="single-info"><span>Email:</span><p><a href="mailto:schandpasha38@gmail.com" target="_blank" rel="noopener noreferrer">schandpasha38@gmail.com</a></p></div>
+                                </div>
+                                <div className="about-text-info-row">
+                                    <div className="single-info"><span>Age:</span><p>30</p></div>
+                                    <div className="single-info"><span>From:</span><p>Bengaluru</p></div>
+                                </div>
+                            </div>
+                            <div className="about-text-social">
+                                <a className="download-cv" href="/">Download CV</a>
+                                <div className="about-social">
+                                    <li><a href="https://www.github.com/schandpasha38" target="_blank" rel="noopener noreferrer"><img className="socialIcons" src={gitIcon} alt="" /></a></li>
+                                    <li><a href="https://www.linkedin.com/in/chand-pasha-b6433889" target="_blank" rel="noopener noreferrer"><img className="socialIcons" src={linkedInIcon} alt="" /></a></li>
+                                    <li><a href="https://www.twitter.com/itschandpasha" target="_blank" rel="noopener noreferrer"><img className="socialIcons" src={twitterIcon} alt="" /></a></li>
+                                    <li><a href="mailto:schandpasha38@gmail.com" target="_blank" rel="noopener noreferrer"><img className="socialIcons" src={eMialIcon} alt="" /></a></li>
+                                </div>
                             </div>
                         </section>
                     </div>
 
                     <div className="about-section-toolsused">
                         <ul>
-                            <li><img src={HTML} alt=""/></li>
-                            <li><img src={CSS} alt=""/></li>
-                            <li><img src={SASS} alt=""/></li>
-                            <li><img src={BOOTSTRAP} alt=""/></li>
-                            <li><img src={JAVASCRIPT} alt=""/></li>
-                            <li><img src={JQUERY} alt=""/></li>
-                            <li><img src={REACT} alt=""/></li>
-                            <li><img src={NODEJS} alt=""/></li>
+                            <li><img src={HTML} alt="" /></li>
+                            <li><img src={CSS} alt="" /></li>
+                            <li><img src={SASS} alt="" /></li>
+                            <li><img src={BOOTSTRAP} alt="" /></li>
+                            <li><img src={JAVASCRIPT} alt="" /></li>
+                            <li><img src={JQUERY} alt="" /></li>
+                            <li><img src={REACT} alt="" /></li>
+                            <li><img src={NODEJS} alt="" /></li>
                         </ul>
                     </div>
                 </div>
--- src/Components/HomeBanner/HomeBanner.js
+++ src/Components/HomeBanner/HomeBanner.js
@@ -8,14 +8,12 @@ function HomeBanner() {
             <div className="homeBanner-container">
                 <div className="homeBanner-left-text">
                     <h3>HEY THERE !</h3>
-                    <h1>Hi, I'm <br/> {`<Chand Pasha />`}</h1>
+                    <h1>Hi, I'm <br /> {`<Chand Pasha />`}</h1>
                     <h5>Front-End Developer in Bangalore</h5>
                     <a className="primary_btn" href="/portfolio"><span>View My Resume</span></a>
                 </div>
                 <div className="homeBanner-left-img">
-                    <div>
-                        <img src={HeroChandPic} alt="" />
-                    </div>
+                    <img src={HeroChandPic} alt="" />
                 </div>
             </div>
         </section>
